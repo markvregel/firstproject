a59bea4993e163098a354f5ddcced54c1a5879c4
user <m.vregelaar@gmail.com>
2016-01-07 11:40:59 +0100

    changed the message in hello world and used the minusrasterfunstion in main( r3=r-r3)

--- R/HelloWorld.R
+++ R/HelloWorld.R
@@ -1,6 +1,6 @@
 HelloWorld <- function (x) {
 	if (is.character(x)) {
-		hello <- sprintf('Hello %s', x)
+		hello <- sprintf('Hello %s, you better start working', x)
 	} else {
 		hello <- warning('Object of class character expected for x')
 	}
